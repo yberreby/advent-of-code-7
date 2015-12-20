db27ff8fbf86c7fe6a5f5902c070b4480c772493
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 12:47:38 +0100

    WIP parser, add test_parse_value_integer

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -22,26 +22,67 @@ pub enum Operation {
     Not(Value),
 }
 
+
 struct Parser<'input> {
     tokens: Vec<Token<'input>>,
+    idx: usize,
 }
 
 impl<'input> Parser<'input> {
     pub fn parse(&mut self) -> Vec<Instruction> {
-        unimplemented!()
+        let mut instructions = Vec::new();
+
+        while let Some(instruction) = self.parse_instruction() {
+            instructions.push(instruction);
+        }
+
+        instructions
+    }
+
+    fn current_token(&self) -> Option<&Token<'input>> {
+        self.tokens.get(self.idx)
+    }
+
+    fn bump(&mut self) {
+        self.idx += 1
+    }
+
+    fn idx(&self) -> usize {
+        self.idx
     }
 
     fn parse_instruction(&mut self) -> Option<Instruction> {
+        let value = self.parse_value();
+
+        // assignment arrow
+
+        // output wire
+
         unimplemented!()
     }
 
+
+
     fn parse_value(&mut self) -> Option<Value> {
-        unimplemented!()
+        match self.current_token() {
+            Some(&Token::Integer(x)) => {
+                self.bump();
+                match self.current_token() {
+                    Some(&Token::AssignmentArrow) => Some(Value::Integer(x)),
+                    // Some(&Token::Operator(op)) => {}
+                    _ => panic!(),
+                }
+            }
+            _ => panic!(),
+        }
     }
 }
 
 pub fn parse<'input>(tokens: Vec<Token<'input>>) -> Vec<Instruction> {
-    let mut parser = Parser { tokens: tokens };
+    let mut parser = Parser {
+        tokens: tokens,
+        idx: 0,
+    };
     parser.parse()
 }
 
@@ -49,6 +90,17 @@ pub fn parse<'input>(tokens: Vec<Token<'input>>) -> Vec<Instruction> {
 mod tests {
     use lexer::Token;
     use super::*;
+    use super::Parser;
+
+    #[test]
+    fn test_parse_value_integer() {
+        let mut parser = Parser {
+            tokens: vec![Token::Integer(123), Token::AssignmentArrow, Token::Identifier("aa")],
+            idx: 0,
+        };
+
+        assert_eq!(parser.parse_value(), Some(Value::Integer(123)));
+    }
 
     #[test]
     fn test_parse_simple_instruction() {
