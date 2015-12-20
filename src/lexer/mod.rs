7ececfcedb3711a6642217b73a6ecc32869c29c6
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 11:12:15 +0100

    add identifier lexing

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -4,9 +4,9 @@ mod utils;
 use self::utils::*;
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Token {
+pub enum Token<'input> {
     Integer(u16),
-    Identifier(String),
+    Identifier(&'input str),
     Operator(Operator),
     AssignmentArrow,
     Newline,
@@ -31,20 +31,20 @@ enum State {
 }
 
 
-pub fn lex(input: &[u8]) -> Vec<Token> {
+pub fn lex<'input>(input: &'input [u8]) -> Vec<Token<'input>> {
     let mut lexer = Lexer::new(input);
     lexer.lex()
 }
 
 impl<'input> Lexer<'input> {
-    pub fn new(input: &[u8]) -> Lexer {
+    pub fn new(input: &'input [u8]) -> Lexer<'input> {
         Lexer {
             buffer: input,
             idx: 0,
         }
     }
 
-    fn lex(&mut self) -> Vec<Token> {
+    fn lex(&mut self) -> Vec<Token<'input>> {
         let mut tokens = Vec::new();
 
         while let Some(token) = self.read_token() {
@@ -64,7 +64,7 @@ impl<'input> Lexer<'input> {
         self.idx += 1
     }
 
-    fn read_token(&mut self) -> Option<Token> {
+    fn read_token(&mut self) -> Option<Token<'input>> {
         // Ignore leading spaces.
         while let Some(b' ') = self.current_char() {
             self.bump();
@@ -80,13 +80,25 @@ impl<'input> Lexer<'input> {
                 }
 
                 let num_buf = &self.buffer[start_idx..self.idx];
-                let num_str = ::std::str::from_utf8(num_buf).unwrap();
 
                 // It is safe to unwrap here, because we just checked the buffer was all
                 // ASCII digits.
+                let num_str = ::std::str::from_utf8(num_buf).unwrap();
+
+                // Same here.
                 let num = num_str.parse()
                                  .unwrap();
                 return Some(Token::Integer(num));
+            }
+            Some(b'a'...b'z') => {
+                while let Some(b'a'...b'z') = self.current_char() {
+                    self.bump();
+                }
+
+                let identifier_buf = &self.buffer[start_idx..self.idx];
+                let identifier_str = ::std::str::from_utf8(identifier_buf).unwrap();
+
+                return Some(Token::Identifier(identifier_str));
             }
             Some(c) => panic!(),
             None => return None,
@@ -114,5 +126,12 @@ mod tests {
         assert_eq!(lex(b"123"), vec![Token::Integer(123)]);
         // u16::MAX
         assert_eq!(lex(b"65535"), vec![Token::Integer(65535)]);
+    }
+
+    #[test]
+    fn lex_identifiers() {
+        assert_eq!(lex(b"ax"), vec![Token::Identifier("ax")]);
+        assert_eq!(lex(b"fu"), vec![Token::Identifier("fu")]);
+        assert_eq!(lex(b"yz"), vec![Token::Identifier("yz")]);
     }
 }
