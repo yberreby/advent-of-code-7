27d6f6d552a70abcecb4736f8152b1450867e0ad
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 14:30:25 +0100

    get constant rshift instruction evaluation working

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -53,7 +53,7 @@ pub fn run(instructions: Vec<Instruction>) -> HashMap<String, u16> {
 
 mod tests {
     use super::*;
-    use parser::{Instruction, Value};
+    use parser::{Instruction, Value, Operation, Operand};
     use std::collections::HashMap;
 
     #[test]
@@ -64,6 +64,20 @@ mod tests {
         assert_eq!(run(vec![Instruction {
                                 input: Value::Integer(45),
                                 output_wire: "ax".into(),
+                            }]),
+                   expected);
+    }
+
+    #[test]
+    fn test_run_single_rshift_instruction() {
+        let mut expected: HashMap<String, u16> = HashMap::new();
+        expected.insert("zz".into(), 30);
+
+        assert_eq!(run(vec![Instruction {
+                                input: Value::Operation(Box::new(Operation::Rshift(
+                                                Operand::Integer(123),
+                                                Operand::Integer(2)))),
+                                output_wire: "zz".into(),
                             }]),
                    expected);
     }
