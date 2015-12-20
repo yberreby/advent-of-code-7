dbff050da2eb9579a19cdb3cef73c95d2b57ed5e
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 15:22:28 +0100

    add part two

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate aoc_7_compiler;
 
 const SOURCE: &'static str = include_str!("../source.txt");
+const SOURCE_2: &'static str = include_str!("../source2.txt");
 
 fn main() {
     let output = aoc_7_compiler::run_source(SOURCE);
     println!("{:?}", output);
 
     println!("Wire 'a' signal: {}", output.get("a").unwrap());
+
+    // Part two.
+    let output2 = aoc_7_compiler::run_source(SOURCE_2);
+    println!("New wire 'a' signal after override: {}",
+             output2.get("a").unwrap());
 }
