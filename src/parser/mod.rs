80c2ed5d64be578cfa8646b2a9e1b117173842d8
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 13:26:51 +0100

    expand parser and add test for NOT instruction

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -23,11 +23,11 @@ pub enum Operand {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Operation {
-    Rshift(Value, Value),
-    Lshift(Value, Value),
-    Or(Value, Value),
-    And(Value, Value),
-    Not(Value),
+    Rshift(Operand, Operand),
+    Lshift(Operand, Operand),
+    Or(Operand, Operand),
+    And(Operand, Operand),
+    Not(Operand),
 }
 
 use std::cell::Cell;
@@ -97,30 +97,80 @@ impl<'input> Parser<'input> {
 
     fn parse_value(&self) -> Option<Value> {
         match self.current_token() {
-            Some(&Token::Integer(x1)) => {
-                self.bump();
+            Some(&Token::Integer(_)) | Some(&Token::Identifier(_)) => {
+                let a = self.parse_operand().unwrap();
+
                 match self.current_token() {
-                    Some(&Token::AssignmentArrow) => Some(Value::Integer(x1)),
-                    Some(&Token::Operator(Operator::Lshift)) => {
+                    Some(&Token::Operator(ref operator)) => {
                         self.bump();
 
-                        match self.current_token() {
-                            Some(&Token::Integer(x2)) => {
-                                Some(Value::Operation(Box::new(Operation::Lshift(Value::Integer(x1), Value::Integer(x2)))))
-                            }
+                        let b = self.parse_operand().unwrap();
+
+                        let operation = match *operator {
+                            Operator::Lshift => Operation::Lshift(a, b),
+                            Operator::Rshift => Operation::Rshift(a, b),
+                            Operator::And => Operation::And(a, b),
+                            Operator::Or => Operation::Or(a, b),
                             _ => panic!(),
+                        };
+
+                        Some(Value::Operation(Box::new(operation)))
+                    }
+                    Some(&Token::AssignmentArrow) => {
+                        match a {
+                            Operand::Integer(x) => Some(Value::Integer(x)),
+                            Operand::Wire(x) => Some(Value::Wire(x)),
                         }
                     }
-                    _ => panic!(),
+                    other => {
+                        panic!("expected an operator or an assignment arrow, found {:?}",
+                               other)
+                    }
                 }
+            }
+            Some(&Token::Operator(Operator::Not)) => {
+                self.bump();
+
+                let a = self.parse_operand().unwrap();
+                Some(Value::Operation(Box::new(Operation::Not(a))))
             }
             None => None,
             other => panic!("unexpected {:?}", other),
         }
+
+        // match self.current_token() {
+        //     Some(&Token::Integer(x1)) => {
+        //         self.bump();
+        //         match self.current_token() {
+        //             Some(&Token::AssignmentArrow) => Some(Value::Integer(x1)),
+        //             Some(&Token::Operator(Operator::Lshift)) => {
+        //                 self.bump();
+
+        //                 match self.current_token() {
+        //                     Some(&Token::Integer(x2)) => {
+        //                         Some(Value::Operation(Box::new(Operation::Lshift(Value::Integer(x1), Value::Integer(x2)))))
+        //                     }
+        //                     _ => panic!(),
+        //                 }
+        //             }
+        //             _ => panic!(),
+        //         }
+        //     }
+        //     None => None,
+        //     other => panic!("unexpected {:?}", other),
+        // }
     }
 
     fn parse_operand(&self) -> Option<Operand> {
-        unimplemented!()
+        let op = match self.current_token() {
+            Some(&Token::Integer(x)) => Some(Operand::Integer(x)),
+            Some(&Token::Identifier(id)) => Some(Operand::Wire(id.into())),
+            _ => panic!(),
+        };
+
+        self.bump();
+
+        op
     }
 }
 
@@ -132,6 +182,7 @@ pub fn parse<'input>(tokens: Vec<Token<'input>>) -> Vec<Instruction> {
 
 mod tests {
     use lexer::Token;
+    use lexer::Operator;
     use super::*;
     use super::Parser;
 
@@ -145,7 +196,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_simple_instruction() {
+    fn test_parse_constant_assignment_instruction() {
         let tokens = vec![Token::Integer(123), Token::AssignmentArrow, Token::Identifier("ax")];
 
         assert_eq!(parse(tokens),
@@ -155,4 +206,17 @@ mod tests {
                         }]);
     }
 
+    #[test]
+    fn test_parse_not_instruction() {
+        let tokens = vec![Token::Operator(Operator::Not),
+                          Token::Integer(123),
+                          Token::AssignmentArrow,
+                          Token::Identifier("ax")];
+
+        assert_eq!(parse(tokens),
+                   vec![Instruction {
+                            input: Value::Operation(Box::new(Operation::Not(Operand::Integer(123)))),
+                            output_wire: "ax".into(),
+                        }]);
+    }
 }
