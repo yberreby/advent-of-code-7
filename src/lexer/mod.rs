a8a6b9243ea9923ee48823f8f638c0f41292cb63
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-20 11:55:49 +0100

    change lex() to take a &str instead of &[u8]

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -31,8 +31,8 @@ enum State {
 }
 
 
-pub fn lex<'input>(input: &'input [u8]) -> Vec<Token<'input>> {
-    let mut lexer = Lexer::new(input);
+pub fn lex<'input>(input: &'input str) -> Vec<Token<'input>> {
+    let mut lexer = Lexer::new(input.as_bytes());
     lexer.lex()
 }
 
@@ -143,58 +143,58 @@ mod tests {
 
     #[test]
     fn lex_newline() {
-        assert_eq!(lex(b"\n"), vec![Token::Newline]);
+        assert_eq!(lex("\n"), vec![Token::Newline]);
     }
 
     #[test]
     fn lex_numbers() {
-        assert_eq!(lex(b"1"), vec![Token::Integer(1)]);
-        assert_eq!(lex(b"123"), vec![Token::Integer(123)]);
+        assert_eq!(lex("1"), vec![Token::Integer(1)]);
+        assert_eq!(lex("123"), vec![Token::Integer(123)]);
         // u16::MAX
-        assert_eq!(lex(b"65535"), vec![Token::Integer(65535)]);
+        assert_eq!(lex("65535"), vec![Token::Integer(65535)]);
     }
 
     #[test]
     fn lex_identifiers() {
-        assert_eq!(lex(b"ax"), vec![Token::Identifier("ax")]);
-        assert_eq!(lex(b"fu"), vec![Token::Identifier("fu")]);
-        assert_eq!(lex(b"yz"), vec![Token::Identifier("yz")]);
+        assert_eq!(lex("ax"), vec![Token::Identifier("ax")]);
+        assert_eq!(lex("fu"), vec![Token::Identifier("fu")]);
+        assert_eq!(lex("yz"), vec![Token::Identifier("yz")]);
     }
 
     #[test]
     fn lex_operators() {
-        assert_eq!(lex(b"NOT"), vec![Token::Operator(Operator::Not)]);
-        assert_eq!(lex(b"AND"), vec![Token::Operator(Operator::And)]);
-        assert_eq!(lex(b"OR"), vec![Token::Operator(Operator::Or)]);
-        assert_eq!(lex(b"LSHIFT"), vec![Token::Operator(Operator::Lshift)]);
-        assert_eq!(lex(b"RSHIFT"), vec![Token::Operator(Operator::Rshift)]);
+        assert_eq!(lex("NOT"), vec![Token::Operator(Operator::Not)]);
+        assert_eq!(lex("AND"), vec![Token::Operator(Operator::And)]);
+        assert_eq!(lex("OR"), vec![Token::Operator(Operator::Or)]);
+        assert_eq!(lex("LSHIFT"), vec![Token::Operator(Operator::Lshift)]);
+        assert_eq!(lex("RSHIFT"), vec![Token::Operator(Operator::Rshift)]);
 
     }
 
     #[test]
     fn lex_assignment_arrow() {
-        assert_eq!(lex(b"->"), vec![Token::AssignmentArrow]);
+        assert_eq!(lex("->"), vec![Token::AssignmentArrow]);
     }
 
     #[test]
     fn lex_simple_instructions() {
-        assert_eq!(lex(b"123 -> ax"),
+        assert_eq!(lex("123 -> ax"),
                    vec![Token::Integer(123), Token::AssignmentArrow, Token::Identifier("ax")]);
 
-        assert_eq!(lex(b"yu -> bp"),
+        assert_eq!(lex("yu -> bp"),
                    vec![Token::Identifier("yu"), Token::AssignmentArrow, Token::Identifier("bp")]);
     }
 
     #[test]
     fn lex_compound_instructions() {
-        assert_eq!(lex(b"x LSHIFT 2 -> f"),
+        assert_eq!(lex("x LSHIFT 2 -> f"),
                    vec![Token::Identifier("x"),
                         Token::Operator(Operator::Lshift),
                         Token::Integer(2),
                         Token::AssignmentArrow,
                         Token::Identifier("f")]);
 
-        assert_eq!(lex(b"NOT x -> h"),
+        assert_eq!(lex("NOT x -> h"),
                    vec![Token::Operator(Operator::Not),
                         Token::Identifier("x"),
                         Token::AssignmentArrow,
@@ -222,6 +222,6 @@ lx -> a";
             Token::Identifier("lx"), Token::AssignmentArrow, Token::Identifier("a"),
         ];
 
-        assert_eq!(lex(program.as_bytes()), expected);
+        assert_eq!(lex(program), expected);
     }
 }
