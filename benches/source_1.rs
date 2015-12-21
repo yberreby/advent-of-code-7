2594de63cc87e500003901e10ef6a668296b2c9b
Yohai Berreby (filsmick) <yohaiberreby@gmail.com>
2015-12-21 12:50:32 +0100

    remove commented out code

--- benches/source_1.rs
+++ benches/source_1.rs
@@ -18,16 +18,3 @@ fn bench_source_1(b: &mut Bencher) {
 fn bench_source_2(b: &mut Bencher) {
     b.iter(|| aoc_7_compiler::run_source(SOURCE_2));
 }
-
-
-// fn main() {
-//     let output = aoc_7_compiler::run_source(SOURCE);
-//     println!("{:?}", output);
-//
-//     println!("Wire 'a' signal: {}", output.get("a").unwrap());
-//
-//     // Part two.
-//     let output2 = aoc_7_compiler::run_source(SOURCE_2);
-//     println!("New wire 'a' signal after override: {}",
-//              output2.get("a").unwrap());
-// }
